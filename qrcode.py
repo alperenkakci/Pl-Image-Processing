c402fb758e3495845976452246ce45ef480ea1bf
Valuenxx <kakcialperen@gmail.com>
2022-01-16 23:28:57 +0300

    QrCode
    QrCode

---
+++ qrcode.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+qr = cv2.imread("qr-code-kirp.png")
+code = decode(qr)
+
+
+for text in code:
+
+    print(text.data)
+    mydata=text.data
+
+mydata=str(mydata)
+mydataa=mydata.strip("'b")
+print(mydataa)
+a=mydataa.find("[")
+b=mydataa.find("]")
+anahtar=mydataa[(a+1):b]
+testsayi=mydataa[0:(a)]
+print(anahtar)
+print(testsayi)
+
+
+
+
+
+
+
+
+
+
+
+#cv2.imshow("Windows",qr)
+#cv2.waitKey(0)
+
